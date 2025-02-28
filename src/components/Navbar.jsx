a5a86999b693214ab8fb187b86349325fa75e930
Yashas Sharma <yashas135790@gmail.com>
2025-03-01 00:00:50 +0530

    update navbar export

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,15 @@
-//rafce
-
-import React from 'react'
-
-const navbar = () => {
+const Navbar = () => {
   return (
     <nav className='flex justify-around bg-slate-800 text-white py-2'>
-    <div className="logo">
-        <span className='font-bold text-xl mx-8'>Task Manager</span>
-    </div>
-  <ul className="flex gap-8 mx-9">
-    <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-    <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
-  </ul>
-</nav>
+      <div className="logo">
+          <span className='font-bold text-xl mx-8'>Task Manager</span>
+      </div>
+      <ul className="flex gap-8 mx-9">
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+      </ul>
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar;
